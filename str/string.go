f22e6669f698465a4981e5307c8b2ab231b30691
废墟 <r.anerg@gmail.com>
2023-04-24 22:30:57 +0800

    中文排序

--- str/string.go
+++ str/string.go
@@ -207,6 +207,26 @@ func HasHan(str string) bool {
 	return false
 }
 
+// 中文按拼音排序
+type SortHans []string
+
+func (s SortHans) Len() int      { return len(s) }
+func (s SortHans) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s SortHans) Less(i, j int) bool {
+	a, _ := UTF82GBK(s[i])
+	b, _ := UTF82GBK(s[j])
+	bLen := len(b)
+	for idx, chr := range a {
+		if idx > bLen-1 {
+			return false
+		}
+		if chr != b[idx] {
+			return chr < b[idx]
+		}
+	}
+	return true
+}
+
 // UTF82GBK : transform UTF8 rune into GBK byte array
 func UTF82GBK(src string) ([]byte, error) {
 	GB18030 := simplifiedchinese.All[0]
